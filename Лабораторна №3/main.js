a8c2edcaff986192b0d025e1d5095b6d13072474
Mykyta <golovakin.kinds@gmail.com>
2020-02-23 14:53:20 +0200

    laba3

---
+++ Лабораторна №3/main.js
@@ -0,0 +1,24 @@
+const cards = document.querySelectorAll('.card');
+
+for (let i =0; i < cards.length; i++){
+    const card = cards[i];
+    card.addEventListener('mousemove', startrotate);
+    card.addEventListener('mouseout', stoprotate);
+}
+    
+
+function startrotate(event){
+   const cardItem = this.querySelector('.card-item');
+    const halfHeight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+   cardItem.style.transform = 
+   'rotateX('+-(event.offsetY - halfHeight)/10+'deg) rotateY('+(event.offsetX - halfWidth)/10+'deg)';
+}
+
+
+function stoprotate(event){
+    const cardItem = this.querySelector('.card-item');
+ 
+    cardItem.style.transform = 
+    'rotate(0)';
+ }
